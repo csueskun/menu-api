c5ba448155c28729cfabb54b05e92f3f8bf6a055
csuescun <csuescun@campus.udes.edu.co>
2021-11-06 16:27:54 -0500

    Admin 2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            NegociosTableSeeder::class,
-            UsersTableSeeder::class,
+            // NegociosTableSeeder::class,
+            // UsersTableSeeder::class,
+            ProductoTipoSeeder::class,
+            ProductoSeeder::class,
         ]);
     }
 }
